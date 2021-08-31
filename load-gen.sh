ad6074a39f1bf5c932aa8c3d6e4a8b751fa74bdf
Bright Zheng <bright.zheng@outlook.com>
2021-08-31 12:16:28 +0800

    add a simple load gen

---
+++ load-gen.sh
@@ -0,0 +1,38 @@
+# simple load gen scripts
+
+#
+# Typical Usage:
+# 
+# 1. Port forward
+#   kubectl port-forward -n demo springboot-swagger-jpa-stack-668d8d5d56-96jrt 8080:8080
+# 
+#
+
+url="http://localhost:8080"
+if ! [[ -z $1 ]]; then
+    url=$1
+fi
+
+
+# DELETE it if any
+curl -i -X DELETE "$url/api/v1/students/90001"
+
+while true; do
+    sleep 0.5
+    
+    # POST to create a new record
+    curl -i -X POST "$url/api/v1/students" \
+        -H "Content-Type: application/json" \
+        --data '{"id":90001,"nid":"test","name":"test"}'
+
+    # GET it back, without hitting the database directly!
+    curl -i -X GET "$url/api/v1/students/90001"
+
+    # PUT to update this record
+    curl -i -X PUT "$url/api/v1/students" \
+        -H "Content-Type: application/json" \
+        --data '{"id":90001,"nid":"test2","name":"test2"}'
+
+    # GET it back, again, without hitting the database directly!
+    curl -X GET "$url/api/v1/students/90001"
+done
