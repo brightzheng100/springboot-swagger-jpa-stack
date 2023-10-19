1663d57d987aa2fcea58f2cc91969727a3a77fba
Bright Zheng <bright.zheng@outlook.com>
2023-10-19 20:31:23 +0800

    extend SpringBootServletInitializer to make jboss happy

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -3,9 +3,10 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
